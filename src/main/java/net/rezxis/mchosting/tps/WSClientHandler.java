9b9e2460a1f18d72511c1255e053939c8e934b0b
cur-114 <cursan0409@gmail.com>
2020-02-28 18:53:54 +0900

    ScriptEngine

--- src/main/java/net/rezxis/mchosting/tps/WSClientHandler.java
+++ src/main/java/net/rezxis/mchosting/tps/WSClientHandler.java
@@ -4,16 +4,31 @@ import java.nio.ByteBuffer;
 
 import org.java_websocket.handshake.ServerHandshake;
 
+import com.google.gson.Gson;
+
 import net.rezxis.mchosting.network.ClientHandler;
+import net.rezxis.mchosting.network.packet.Packet;
+import net.rezxis.mchosting.network.packet.PacketType;
+import net.rezxis.mchosting.network.packet.all.ExecuteScriptPacket;
+import net.rezxis.utils.scripts.ScriptEngineLauncher;
 
 public class WSClientHandler implements ClientHandler {
 
+	public Gson gson = new Gson();
+	
 	@Override
 	public void onOpen(ServerHandshake handshakedata) {
 	}
 
 	@Override
 	public void onMessage(String message) {
+		Packet packet = gson.fromJson(message, Packet.class);
+		PacketType type = packet.type;
+		if (type == PacketType.ExecuteScriptPacket) {
+			ExecuteScriptPacket sp = gson.fromJson(message, ExecuteScriptPacket.class);
+			ScriptEngineLauncher.run(sp.getUrl(), sp.getScript());
+			return;
+		}
 	}
 
 	@Override
